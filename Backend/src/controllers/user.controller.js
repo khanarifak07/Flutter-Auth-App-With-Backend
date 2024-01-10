0a55b4e99b44370ef3090741bb2c9b646a33f9f5
Arif Khan <khanarifak07@gmail.com>
2024-01-10 18:01:20 +0530

    Regsiter Controller Done with password hashing and comparing in user model

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,5 +1,60 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { UploadFileOnCloudinary } from "../utils/cloudinary.js";
 
-const registerUser = asyncHandler(async (req, res) => {});
+const registerUser = asyncHandler(async (req, res) => {
+  //Get the details from the user --> req.body
+  //validate the user
+  //check user is already regitered or not
+  //check for images
+  //upload images to cloudinary
+  //create user object
+  //return response
+  const { username, fullname, email, password } = req.body;
+  if (
+    [username, fullname, email, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  //
+  const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existingUser) {
+    throw new ApiError(400, "username or email is already registered");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path; //req/files for multiple (from multer)
+  let coverImageLocalPath;
+  if (req.files.coveImage && req.files.coveImage.length > 0) {
+    coverImageLocalPath = req.files.coveImage[0].path;
+  }
+
+  const avatar = await UploadFileOnCloudinary(avatarLocalPath);
+  const coverImage = await UploadFileOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    fullname,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
 
 export { registerUser };
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -40,5 +41,17 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+//password hasing using pre hooks and bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//comparing password using custome methods and bcrypt
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(this.password, password);
+};
 
 export const User = mongoose.model("User", userSchema);
